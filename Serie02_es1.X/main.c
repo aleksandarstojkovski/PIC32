8dec8c71afeb55b8e5d474deb8912f677a152616
Aleksandar Stojkovski <aleksandar@stojkovski.ch>
2019-10-06 13:13:14 +0200

    Serie 2 es 1

--- Serie02_es1.X/main.c
+++ Serie02_es1.X/main.c
@@ -58,6 +58,7 @@ int main(int argc, char** argv) {
     
     while (1){
         
+        // LED 0
         if (PORTFbits.RF3 == 1 && PORTAbits.RA0 == 0){
             LATAbits.LATA0=1;
             utils_uart_putU4_string("LED0 - ON\r\n");
@@ -66,6 +67,7 @@ int main(int argc, char** argv) {
             utils_uart_putU4_string("LED0 - OFF\r\n");
         }
         
+        // LED 1
         if (PORTFbits.RF5 == 1 && PORTAbits.RA1 == 0){
             LATAbits.LATA1=1;
             utils_uart_putU4_string("LED1 - ON\r\n");
@@ -74,6 +76,7 @@ int main(int argc, char** argv) {
             utils_uart_putU4_string("LED1 - OFF\r\n");
         }
         
+        // LED 2
         if (PORTFbits.RF4 == 1 && PORTAbits.RA2 == 0){
             LATAbits.LATA2=1;
             utils_uart_putU4_string("LED2 - ON\r\n");
@@ -82,6 +85,7 @@ int main(int argc, char** argv) {
             utils_uart_putU4_string("LED2 - OFF\r\n");
         }
         
+        // LED 3
         if (PORTDbits.RD15 == 1 && PORTAbits.RA3 == 0){
             LATAbits.LATA3=1;
             utils_uart_putU4_string("LED3 - ON\r\n");
@@ -90,6 +94,7 @@ int main(int argc, char** argv) {
             utils_uart_putU4_string("LED3 - OFF\r\n");
         }
         
+        // LED 4
         if (PORTDbits.RD14 == 1 && PORTAbits.RA4 == 0){
             LATAbits.LATA4=1;
             utils_uart_putU4_string("LED4 - ON\r\n");
@@ -97,6 +102,33 @@ int main(int argc, char** argv) {
             LATAbits.LATA4=0;
             utils_uart_putU4_string("LED4 - OFF\r\n");
         }
+        
+        // LED 5
+        if (PORTBbits.RB11 == 1 && PORTAbits.RA5 == 0){
+            LATAbits.LATA5=1;
+            utils_uart_putU4_string("LED5 - ON\r\n");
+        } else  if (PORTBbits.RB11 == 0 && PORTAbits.RA5 == 1){
+            LATAbits.LATA5=0;
+            utils_uart_putU4_string("LED5 - OFF\r\n");
+        }
+        
+        // LED 6
+        if (PORTBbits.RB10 == 1 && PORTAbits.RA6 == 0){
+            LATAbits.LATA6=1;
+            utils_uart_putU4_string("LED6 - ON\r\n");
+        } else  if (PORTBbits.RB10 == 0 && PORTAbits.RA6 == 1){
+            LATAbits.LATA6=0;
+            utils_uart_putU4_string("LED6 - OFF\r\n");
+        }
+        
+        // LED 7
+        if (PORTBbits.RB9 == 1 && PORTAbits.RA7 == 0){
+            LATAbits.LATA7=1;
+            utils_uart_putU4_string("LED7 - ON\r\n");
+        } else  if (PORTBbits.RB9 == 0 && PORTAbits.RA7 == 1){
+            LATAbits.LATA7=0;
+            utils_uart_putU4_string("LED7 - OFF\r\n");
+        }        
         
     }
     
